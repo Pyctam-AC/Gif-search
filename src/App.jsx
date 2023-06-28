cce940c540d312a58d42247afb4a14572b9727c2
Augenb1ick <augenblick14@yandex.ru>
2023-06-28 12:49:20 +0400

    vite react template created

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+const App = () => {
+
+  const content = 
+  <div>
+  <h1 className="text-5xl font-bold underline">Hello world</h1>
+  <h2 className="text-3xl font-sans text-red-500">Test</h2>
+  </div>
+
+  return content
+}
+
+export default App
