0d76f42e1a86bd3c68d1b1f82d7965ffb184125c
Augenb1ick <augenblick14@yandex.ru>
2023-06-29 18:47:17 +0400

    fixed bugs

--- src/components/GifCards.jsx
+++ src/components/GifCards.jsx
@@ -1,39 +1,14 @@
 /* eslint-disable react/prop-types */
-import { useEffect, useState } from 'react';
-import { trending, search, random } from '../utills/api';
 
 import Gif from './Gif';
 
 const GifCards = ({ gifCards }) => {
-  /*   const [trendingGifs, setTrendinGifs] = useState([]);
-  const [searchedGifs, serSearchedGifs] = useState([]);
-  const [ramdomGif, setRandomGif] = useState([]);
-
-  useEffect(() => {
-    trending(9).then((res) => {
-      setTrendinGifs(res.data);
-    });
-  }, []);
-
-  useEffect(() => {
-    if (props.searchQuery) {
-      search(props.searchQuery.searchValue, 9).then((res) => {
-        serSearchedGifs(res.data);
-      });
-    }
-  }, [props.searchQuery]);
-
-  useEffect(() => {
-    random().then((res) => {
-      setRandomGif(res.data);
-    });
-  }, []); */
-
+  console.log(gifCards);
   return (
     <section
       className={`${
         typeof gifCards === 'string' ? 'grid-cols-1' : 'grid-cols-3'
-      } mt-10 mb-10 grid grid-cols-3 gap-y-5 gap-x-5 justify-items-center justify-between min-h-screen`}
+      } mt-10 mb-10 grid gap-y-5 gap-x-5 justify-items-center justify-between min-h-screen`}
     >
       {typeof gifCards === 'string' ? (
         <Gif gifImg={gifCards} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,17 +4,7 @@ const Header = () => {
   return (
     <header className='w-100% mt-10 '>
       <img className='w-28 m-auto' src={headerLogo} />
-{/*       <nav className='flex gap-8'>
-        <button className='text-white text-xl hover:text-orange-500'>
-          Поиск
-        </button>
-        <button className='text-white text-xl hover:text-orange-500'>
-          Тренды
-        </button>
-        <button className='text-white text-xl hover:text-orange-500'>
-          Случайный гиф
-        </button>
-      </nav> */}
+      <h1 className='text-white text-2xl text-center'>GIF Search</h1>
     </header>
   );
 };
