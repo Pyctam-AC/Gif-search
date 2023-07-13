359e0720be3aaf8740063905f70e6b990bc7129b
Rustam Sultangaliev <sultangaliev-ru@yandex.ru>
2023-07-14 00:53:52 +0500

    fixed app components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
-import { Route, Routes } from 'react-router-dom';
+import { useEffect } from "react";
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import Header from './Header.jsx';
 //import Main from './Main.jsx';
 import Footer from './Footer.jsx';
@@ -8,13 +9,20 @@ import TrendingGifs from './TrendingGifs.jsx';
 import Search from './Search.jsx';
 
 const App = () => {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate('/search');
+  });
+
   return (
     <div className=' min-h-screen flex flex-col items-center bg-neutral-800'>
       <Header />
       <main className='w-100% mt-10 '>
         <NavPage />
         <Routes>
-          <Route path='/' element={<Search />} />
+          <Route path='/search' element={<Search />} />
           <Route path='/random' element={<RamdomGif />} />
           <Route path='/trending' element={<TrendingGifs />} />
         </Routes>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,6 @@
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import { Routes} from 'react-router-dom';
 import Search from './Search.jsx';
-import GifCards from './GifCards.jsx';
-import { useEffect, useState } from 'react';
+
 import NavPage from './NavPage.jsx';
 import RamdomGif from './RamdomGif.jsx';
 import TrendingGifs from './TrendingGifs.jsx';
@@ -11,7 +10,7 @@ const Main = () => {
     <main className='w-100% mt-10 '>
       <Routes>
         <NavPage />
-        <Routes path='/' element={<Search />} />
+        <Routes path='/search' element={<Search />} />
         <Routes path='/random' element={<RamdomGif />} />
         <Routes path='/trending' element={<TrendingGifs />} />
       </Routes>
